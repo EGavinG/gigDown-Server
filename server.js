6dcff0f37d3acc702606926130034e267e3cbcf8
Gavin Garcia <edwardgavingarcia@yahoo.com>
2024-03-02 22:47:07 -0600

    Continued CORS fix

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.locals.usblibrary = [
     ]
 
     app.use((req, res, next) => {
-      res.setHeader('Access-Control-Allow-Origin', 'http://localhost3000');
+      res.setHeader('Access-Control-Allow-Origin', '*');
       res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
       res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
       next();
